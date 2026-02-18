5ce2e960077d6627fc11d302597a3ede6c932979
shruthik shetty <shrutikshetty14@gmail.com>
2026-02-18 21:52:42 +0530

    add design

--- src/renderer/src/components/Versions.tsx
+++ src/renderer/src/components/Versions.tsx
@@ -4,10 +4,10 @@ function Versions(): React.JSX.Element {
   const [versions] = useState(window.electron.process.versions)
 
   return (
-    <ul className="versions">
-      <li className="electron-version">Electron v{versions.electron}</li>
-      <li className="chrome-version">Chromium v{versions.chrome}</li>
-      <li className="node-version">Node v{versions.node}</li>
+    <ul className="flex flex-row gap-2 bg-gray-800 p-2 rounded-md">
+      <li className="text-sm text-blue-500">Electron v{versions.electron}</li>
+      <li className="text-sm text-blue-500">Chromium v{versions.chrome}</li>
+      <li className="text-sm text-blue-500">Node v{versions.node}</li>
     </ul>
   )
 }
